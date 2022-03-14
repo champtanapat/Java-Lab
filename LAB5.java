62d65c44061d167bda3f3a1c40de5a401cf7e93d
Tanapat Permpoon <champ_vs_the.dancefloor@hotmail.com>
2022-03-14 17:50:38 +0700

    LAB5,6

---
+++ LAB5.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class LAB5 {
+    public static void main(String[] args) {
+        int scoreAssign,scoreReport, scoreMid,scoreFinal; 
+        int sumScore = 0 ; 
+        Scanner input =new Scanner(System.in);
+        
+        System.out.print("Input Score Assignment : ");
+        scoreAssign = input.nextInt();
+        System.out.print("Input Score Report: ");
+        scoreReport = input.nextInt();
+
+        System.out.print("Input Score Midterm: ");
+        scoreMid = input.nextInt();
+
+        System.out.print("Input Score Final: ");
+        scoreFinal = input.nextInt();
+        sumScore = scoreAssign + scoreReport + scoreMid + scoreFinal; 
+        if(sumScore<=100)
+        {
+           if(sumScore>=90){
+            System.out.print("Grade  A");
+           }
+           else if(sumScore>=80){
+            System.out.print("Grade  B");
+           }
+           else if(sumScore>=70){
+            System.out.print("Grade  C");
+           }
+           else if(sumScore>=50){
+            System.out.print("Grade  D");
+           }
+           else{
+            System.out.print("Grade  F");
+           }
+        }
+        else
+        {
+            System.out.print("INVALID SCORE");
+        }
+        
+    }
+}
